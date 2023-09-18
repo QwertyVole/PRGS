ce7cee5daecafe303e2a75cf3e4abea207f4ed2d
QwertyVole <fakeqwertzy@gmail.com>
2023-09-19 01:23:23 +0200

    nevim co se to deje

---
+++ Calculator/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Made by Jan Borecky for PRG seminar at Gymnazium Voderadska, year 2023-2024.
+ * Extended by students.
+ */
+
+namespace Calculator
+{
+    internal class Program
+    {
+
+        static double Plus(a, b){
+            return a + b
+        };
+        static double Minus(a, b){
+            return a - b
+        };
+        static double Multiply(a, b){
+            return a * b
+        };
+        static double Divide(a, b){
+            if(b != 0)
+                return a / b
+            else return null;
+        };
+        static double Power(a, b){
+            return a ^ b
+        };
+        static bool verifyNum(num){
+            if typeof(num) = double
+                return true;
+            else return false;
+        }
+
+        static void Main(string[] args)
+    
+        {
+            double a,b,result = 0;
+            string operation;
+            
+            Console.WriteLine("a");
+            a = Convert.ToDouble(Console.ReadLine);
+            if(verifyNum(b)= false)
+                Console.WriteLine("Thats not a valid number");
+                break;
+
+            Console.WriteLine("Type in the symbol for desired operation operation \n 1)a+b 2)a-b 3)a*b \n 4)a/b 5)a^b");
+            operation = Console.ReadLine();
+            
+            Console.WriteLine("b");
+            b = Convert.ToDouble(Console.ReadLine);
+            if(verifyNum(b)= false)
+                Console.WriteLine("Thats not a valid number");
+                break;
+
+
+            switch(operation){
+                case "+":
+                    result = Plus(a,b);
+                    break;
+                case "-":
+                    result = Minus(a,b);
+                    break;
+                case "*":
+                    result = Multiply(a,b);
+                    break;
+                case "/":
+                    result = Divide(a,b)
+                    break;
+                
+                case "^":
+                    result = Power(a,b);
+                    break;
+                
+                default:
+                    Console.WriteLine("Rozbil si to a ne nemuzu za to ja");
+                    return;
+                    break;
+                };
+            
+
+            Console.WriteLine(result);
+            Console.WriteLine("press any key to exit");
+
+                    
+
+            /*
+             * Pokud se budes chtit na neco zeptat a zrovna budu pomahat jinde, zkus se zeptat ChatGPT ;) - https://chat.openai.com/
+             * 
+             * ZADANI
+             * Vytvor program ktery bude fungovat jako kalkulacka. Kroky programu budou nasledujici:
+             * 1) Nacte vstup pro prvni cislo od uzivatele (vyuzijte metodu Console.ReadLine() - https://learn.microsoft.com/en-us/dotnet/api/system.console.readline?view=netframework-4.8.
+             * 2) Zkonvertuje vstup od uzivatele ze stringu do integeru - https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/types/how-to-convert-a-string-to-a-number.
+             * 3) Nacte vstup pro druhe cislo od uzivatele a zkonvertuje ho do integeru. (zopakovani kroku 1 a 2 pro druhe cislo)
+             * 4) Nacte vstup pro ciselnou operaci. Rozmysli si, jak operace nazves. Muze to byt "soucet", "rozdil" atd. nebo napr "+", "-", nebo jakkoliv jinak.
+             * 5) Nadefinuj integerovou promennou result a prirad ji prozatimne hodnotu 0.
+             * 6) Vytvor podminky (if statement), podle kterych urcis, co se bude s cisly dit podle zadane operace
+             *    a proved danou operaci - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/selection-statements.
+             * 7) Vypis promennou result do konzole
+             * 
+             * Mozna rozsireni programu pro rychliky / na doma (na poradi nezalezi):
+             * 1) Vypis do konzole pred nactenim kazdeho uzivatelova vstupu co po nem chces
+             * 2) a) Kontroluj, ze uzivatel do vstupu zadal, co mel (cisla, popr. ciselnou operaci). Pokud zadal neco jineho, napis mu, co ma priste zadat a program ukoncete.
+             * 2) b) To same, co a) ale misto ukonceni programu opakovane cti vstup, dokud uzivatel nezada to, co ma
+             *       - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/iteration-statements#the-while-statement
+             * 3) Umozni uzivateli zadavat i desetinna cisla, tedy prekopej kalkulacku tak, aby umela pracovat s floaty
+             */
+            Console.ReadKey();
+        }
+    }
+}
